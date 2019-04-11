b4c98cfbe661a8d436524220b2f83351f853a5ef
ziad <ziadmabdelhamed@gmail.com>
2019-04-12 01:30:06 +0200

    add input componenet

--- src/app/forms/shared/input/input.component.ts
+++ src/app/forms/shared/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.scss']
 })
 export class InputComponent implements OnInit {
+  @Input('name') data: string[];
+  @Input('type') type: string[];
 
   constructor() { }
 
