27827bce43132c0166931186f3f3e0d4036f90e1
ziad <ziadmabdelhamed@gmail.com>
2019-04-12 18:08:05 +0200

    login form validation

--- src/app/forms/login-form/login-form.component.ts
+++ src/app/forms/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormControl,Validators} from '@angular/forms';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-form',
@@ -13,6 +14,7 @@ export class LoginFormComponent implements OnInit {
   public type_email: string = "email";
   public email: any = "email";
   public password: any = "password";
+  public errors_array = [];
 
   loginform = new FormGroup({
     email: new FormControl('', [
@@ -26,9 +28,15 @@ export class LoginFormComponent implements OnInit {
 
   public form_name: any = this.loginform.valid;
 
-  constructor() {
+  constructor(private router: Router) {
 
+    var login = localStorage.getItem('login');
 
+    if(!login)
+    {
+      var login_empty = [];
+      localStorage.setItem('login', JSON.stringify(login_empty));
+    }
   }
 
   ngOnInit() {
@@ -37,6 +45,30 @@ export class LoginFormComponent implements OnInit {
 
   login()
   {
+    var login = [];
+
+    var reg_array = JSON.parse(localStorage.getItem('registers'));
+
+    const found = reg_array.some(el => el.email === this.loginform.value.email &&  el.password === this.loginform.value.password);
+
+
+
+    if(found)
+    {
+      this.errors_array = [];
+      login.push(this.loginform.value);
+      localStorage.setItem('login', JSON.stringify(login));
+      this.router.navigate(['/']);
+    }
+
+    else
+    {
+      var error = {
+        "error" : "account not found ",
+      }
+      this.errors_array.push(error);
+    }
+
     console.log(JSON.stringify(this.loginform.value));
   }
 
